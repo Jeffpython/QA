cabc80bce5ea16729ca0efae384ae99665e83bdf
Jeff Minnegaraev <jeffwingchun@mail.ru>
2021-11-30 18:28:05 +0300

    changed SQL_HW_2.sql

--- sql/SQL_HW_2.sql
+++ sql/SQL_HW_2.sql
@@ -1,4 +1,4 @@
--- 1. РЎРѕР·РґР°С‚СЊ С‚Р°Р±Р»РёС†Сѓ employees:
+-- 1. Создать таблицу employees:
 -- - id (serial, primary key),
 -- - employee_name (varchar(50), not null)
 
@@ -7,7 +7,7 @@ CREATE TABLE employees (
 	employee_name VARCHAR(50) UNIQUE NOT NULL
 )
 
--- 2. РќР°РїРѕР»РЅРёС‚СЊ С‚Р°Р±Р»РёС†Сѓ employee 70 СЃС‚СЂРѕРєР°РјРё
+-- 2. Наполнить таблицу employee 70 строками
 
 INSERT INTO employees (employee_name)
 VALUES ('Robert Harris'),
@@ -81,7 +81,7 @@ VALUES ('Robert Harris'),
 	('Michelle Torres'),
 	('Sandra Hunt')
  
--- 3. РЎРѕР·РґР°С‚СЊ С‚Р°Р±Р»РёС†Сѓ salary:
+-- 3. Создать таблицу salary:
 -- - id (serial, primary key),
 -- - monthly_salary (int, not null)
 
@@ -90,7 +90,7 @@ CREATE TABLE salary (
 	monthly_salary INT UNIQUE NOT NULL
 )
 
--- 4. РќР°РїРѕР»РЅРёС‚СЊ С‚Р°Р±Р»РёС†Сѓ salary 16 СЃС‚СЂРѕРєР°РјРё
+-- 4. Наполнить таблицу salary 16 строками
 
 INSERT INTO salary (monthly_salary)
 VALUES ('1000'),
@@ -110,7 +110,7 @@ VALUES ('1000'),
 	('2400'),
 	('2500')
 
--- 5. РЎРѕР·РґР°С‚СЊ С‚Р°Р±Р»РёС†Сѓ employee_salary:
+-- 5. Создать таблицу employee_salary:
 -- - id (serial, primary key),
 -- - employee_id (int, not null, unique)
 -- - salary_id (int, not null)
@@ -121,8 +121,8 @@ CREATE TABLE employee_salary (
 	salary_id INT NOT NULL
 )
 
--- 6. РќР°РїРѕР»РЅРёС‚СЊ С‚Р°Р±Р»РёС†Сѓ employee_salary 40 СЃС‚СЂРѕРєР°РјРё:
--- - РІ 10 СЃС‚СЂРѕРє РёР· 40 РІСЃС‚Р°РІРёС‚СЊ РЅРµСЃСѓС‰РµСЃС‚РІСѓСЋС‰РёРµ employee_id
+-- 6. Наполнить таблицу employee_salary 40 строками:
+-- - в 10 строк из 40 вставить несуществующие employee_id
 
 INSERT INTO employee_salary (employee_id, salary_id)
 VALUES ('1', '1'),
@@ -166,7 +166,7 @@ VALUES ('1', '1'),
 	('79', '7'),
 	('80', '8')
 
--- 7. РЎРѕР·РґР°С‚СЊ С‚Р°Р±Р»РёС†Сѓ roles
+-- 7. Создать таблицу roles
 -- - id (serial, primary key),
 -- - role_name (int, not null, unique)
 	
@@ -175,11 +175,11 @@ CREATE TABLE roles (
 	role_name INT UNIQUE NOT NULL
 )
 
--- 8. РџРѕРјРµРЅСЏС‚СЊ С‚РёРї СЃС‚РѕР»Р±Р° role_name СЃ int РЅР° varchar(30)
+-- 8. Поменять тип столба role_name с int на varchar(30)
 
 ALTER TABLE roles ALTER COLUMN role_name TYPE VARCHAR(30)
 
--- 9. РќР°РїРѕР»РЅРёС‚СЊ С‚Р°Р±Р»РёС†Сѓ roles 20 СЃС‚СЂРѕРєР°РјРё
+-- 9. Наполнить таблицу roles 20 строками
 
 INSERT INTO roles
 VALUES (1, 'Junior Python developer'),
@@ -203,10 +203,10 @@ VALUES (1, 'Junior Python developer'),
 	(19, 'Middle Automation QA engineer'),
 	(20, 'Senior Automation QA engineer')
 
--- 10. РЎРѕР·РґР°С‚СЊ С‚Р°Р±Р»РёС†Сѓ roles_employee
+-- 10. Создать таблицу roles_employee
 -- - id (serial, primary key),
--- - employee_id (int, not null, unique (РІРЅРµС€РЅРёР№ РєР»СЋС‡ РґР»СЏ С‚Р°Р±Р»РёС†С‹ employees, РїРѕР»Рµ id))
--- - role_id (int, not null (РІРЅРµС€РЅРёР№ РєР»СЋС‡ РґР»СЏ С‚Р°Р±Р»РёС†С‹ roles, РїРѕР»Рµ id))
+-- - employee_id (int, not null, unique (внешний ключ для таблицы employees, поле id))
+-- - role_id (int, not null (внешний ключ для таблицы roles, поле id))
 
 CREATE TABLE roles_employee (
 	id SERIAL PRIMARY KEY,
@@ -218,7 +218,7 @@ CREATE TABLE roles_employee (
 		REFERENCES roles (id)
 )
 
--- 11. РќР°РїРѕР»РЅРёС‚СЊ С‚Р°Р±Р»РёС†Сѓ roles_employee 40 СЃС‚СЂРѕРєР°РјРё
+-- 11. Наполнить таблицу roles_employee 40 строками
 
 INSERT INTO roles_employee (employee_id, role_id)
 VALUES (7, 2),
