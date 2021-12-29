a6fc9688611f4df7d29f6cf5304400df005284ce
Jeff Minnegaraev <jeffwingchun@mail.ru>
2021-12-29 21:23:32 +0300

    Changed SQL_excersises.sql

--- sql/SQL_excersises.sql
+++ sql/SQL_excersises.sql
@@ -1,3 +1,20 @@
+Найдите номера моделей и цены всех имеющихся в продаже продуктов (любого типа) производителя B (латинская буква).
+
+SELECT DISTINCT pc.model, pc.price
+FROM product
+JOIN pc ON pc.model = product.model
+WHERE product.maker = 'B'
+UNION
+SELECT DISTINCT laptop.model, laptop.price
+FROM product
+JOIN laptop ON laptop.model = product.model
+WHERE product.maker = 'B'
+UNION
+SELECT DISTINCT printer.model, printer.price
+FROM product
+JOIN printer ON printer.model = product.model
+WHERE product.maker = 'B'
+
 Найдите производителя, выпускающего компьютеры, но не ноутбуки.
 
 SELECT maker
